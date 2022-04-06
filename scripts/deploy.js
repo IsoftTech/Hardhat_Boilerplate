5c9fb28d800e70dc969c2a21bc174307d0a4198b
isofttechn <isofttechn@gmail.com>
2022-04-07 00:56:13 +0100

    Update commits

---
+++ scripts/deploy.js
@@ -0,0 +1,32 @@
+// We require the Hardhat Runtime Environment explicitly here. This is optional
+// but useful for running the script in a standalone fashion through `node <script>`.
+//
+// When running the script with `npx hardhat run <script>` you'll find the Hardhat
+// Runtime Environment's members available in the global scope.
+const hre = require("hardhat");
+
+async function main() {
+  // Hardhat always runs the compile task when running scripts with its command
+  // line interface.
+  //
+  // If this script is run directly using `node` you may want to call compile
+  // manually to make sure everything is compiled
+  // await hre.run('compile');
+
+  // We get the contract to deploy
+  const TeTherium = await hre.ethers.getContractFactory("TeTherium"); // <-- Change this to your contract name
+  const teTherium = await TeTherium.deploy(); // <-- Change this to your contract constructor arguments and remove "Hello, Hardhat!"
+
+  await teTherium.deployed();
+
+  console.log("TeTherium deployed to:", teTherium.address);
+}
+
+// We recommend this pattern to be able to use async/await everywhere
+// and properly handle errors.
+main()
+  .then(() => process.exit(0))
+  .catch((error) => {
+    console.error(error);
+    process.exit(1);
+  });
---
+++ test/TeTheriumTest.js
@@ -0,0 +1,14 @@
+const { expect } = require("chai");
+const { ethers } = require("hardhat");
+
+describe("TeTherium", function () {
+  it("Should return owner of token", async function () {
+    const {owner} = await ethers.getSigners();
+    const TeTherium = await ethers.getContractFactory("TeTherium");
+    const teTherium = await TeTherium.deploy();
+    await teTherium.deployed();
+const owneraddress = await teTherium.owner();
+
+expect(owneraddress).to.equal(owner.address);
+  });
+});
