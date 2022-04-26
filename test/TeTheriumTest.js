099405a4b8fbbb2d4bc0513112dcaf05fe1b1760
isofttechn <isofttechn@gmail.com>
2022-04-26 12:24:34 +0100

    Update with config and test file

--- test/TeTheriumTest.js
+++ test/TeTheriumTest.js
@@ -3,7 +3,7 @@ const { ethers } = require("hardhat");
 
 describe("TeTherium", function () {
   it("Should return owner of token", async function () {
-    const {owner} = await ethers.getSigners();
+    const [owner] = await ethers.getSigners();
     const TeTherium = await ethers.getContractFactory("TeTherium");
     const teTherium = await TeTherium.deploy();
     await teTherium.deployed();
