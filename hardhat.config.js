f4c2616e3d2d44cef6c42916ffa946b97000d2c3
isofttechn <isofttechn@gmail.com>
2022-04-26 12:26:39 +0100

    Update with hardhat config and test file

--- hardhat.config.js
+++ hardhat.config.js
@@ -16,6 +16,29 @@ task("accounts", "Prints the list of accounts", async (taskArgs, hre) => {
 /**
  * @type import('hardhat/config').HardhatUserConfig
  */
+
 module.exports = {
-   solidity: "0.6.12",
+  solidity: "0.6.12",
+  networks: {
+    dev: {
+        url: "http://localhost:7545",
+        gasPrice: 20,
+        saveDeployments: true
+    },
+    bsctest: {
+        url: "https://data-seed-prebsc-2-s2.binance.org:8545/",
+        accounts: [process.env.PRIV_KEY],
+        gasPrice: 10000000000,
+        blockGasLimit: 1000000
+    },
+    bsc: {
+        url: "https://bsc-dataseed1.binance.org/",
+        accounts: [process.env.PRIV_KEY],
+        gasPrice: 5100000000,
+        blockGasLimit: 1000000
+    }
+},
+etherscan:{
+  apiKey:process.env.API_KEY
+}
 };
